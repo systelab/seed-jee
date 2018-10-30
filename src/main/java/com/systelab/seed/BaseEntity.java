533ae34965bfd7ba31967c2a634952100f4be9ce
vinyulis <vinyulis@gmail.com>
2018-10-30 16:14:21 +0100

    Add @XmlTransient annotation to creationTimne and  updateTime (#34)

--- src/main/java/com/systelab/seed/BaseEntity.java
+++ src/main/java/com/systelab/seed/BaseEntity.java
@@ -9,6 +9,7 @@ import javax.persistence.Column;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
+import javax.xml.bind.annotation.XmlTransient;
 import java.sql.Timestamp;
 import java.util.UUID;
 
@@ -23,9 +24,11 @@ public abstract class BaseEntity {
     protected UUID id;
 
     @CreationTimestamp
+    @XmlTransient
     protected Timestamp creationTime;
 
     @UpdateTimestamp
+    @XmlTransient
     protected Timestamp updateTime;
 
 }
