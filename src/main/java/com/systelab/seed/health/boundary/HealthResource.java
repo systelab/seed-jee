6f51a0afea0fb279da3e57decd5890b97c92a0e0
alfonsserra <alfonsoserra@gmail.com>
2018-10-30 14:14:47 +0100

    Repackage Health Resource

--- src/main/java/com/systelab/seed/health/boundary/HealthResource.java
+++ src/main/java/com/systelab/seed/health/boundary/HealthResource.java
@@ -1,4 +1,4 @@
-package com.systelab.seed.health;
+package com.systelab.seed.health.boundary;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
