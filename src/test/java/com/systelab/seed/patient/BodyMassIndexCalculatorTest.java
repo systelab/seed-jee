90899c18dfff343cc180a969a4ecf291fd47cc54
alfonsserra <alfonsoserra@gmail.com>
2018-11-05 22:05:50 +0100

    Use JUnit 5 for the parametrized test

--- src/test/java/com/systelab/seed/patient/BodyMassIndexCalculatorTest.java
+++ src/test/java/com/systelab/seed/patient/BodyMassIndexCalculatorTest.java
@@ -1,81 +1,74 @@
 package com.systelab.seed.patient;
 
-import java.util.Arrays;
-import java.util.Collection;
-
-import org.junit.Test;
-import org.junit.experimental.runners.Enclosed;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
-import org.junit.runners.Parameterized.Parameter;
-import org.junit.runners.Parameterized.Parameters;
-
 import com.systelab.seed.patient.control.BodyMassIndexCalculator;
 import com.systelab.seed.utils.TestUtil;
-
 import io.qameta.allure.Description;
 import io.qameta.allure.Feature;
+import io.qameta.allure.Step;
 import io.qameta.allure.TmsLink;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 
-@RunWith(Enclosed.class)
 public class BodyMassIndexCalculatorTest {
 
-	  @TmsLink("TC1234_Formulas")
-	  @Feature("Goal: This test case is intended to verify the correct calculation of the different Statistical Calculations")
-	  @RunWith(Parameterized.class)
-	  public static class BMICalculationForCorrectValuesTest
-	  {
-	    @Parameter(0)
-		public Double weight;
-	    @Parameter(1)
-	    public Double height;
-	    @Parameter(2)
-	    public Double expectedResult;
+    static class Scenario {
+        public Double weight;
+        public Double height;
+        public Double expectedResult;
 
-	    @Parameters(name = "weight={0},height={1},expectedResult={2}")
-	    public static Collection<Object[]> data()
-	    {
-	      // @formatter:off
-	    	Object[][] data = new Object[][] {
-	            { 0.0, 0.0, null },
-	            { -1.0, 0.0, null },
-	            { -1.0, 1.0, null },
-	            { 1.0, -1.0, null },
-	            { 1.0, 0.0, null },
-	            { 5.0, 0.1, null },
-				{ 30.0, 0.5, 120.0 },
-				{ 45.0, 1.0, 45.0 },
-	            { 65.0, 1.40, 33.2 },
-	            { 75.0, 1.8, 23.1 },
-	            { 95.0, 1.8, 29.3 },
-	            { 78.5, 1.805, 24.1 },
-	          };
-	      // @formatter:on
-	      return Arrays.asList(data);
-	    }
-
-	    @Description("Calculate BMI")
-        @Test
-        public void checkBMICalculationForCorrectValuesTest() {
-	    	Double bmiCalculated = BodyMassIndexCalculator.getBMI(weight, height);
-	    	if (expectedResult==null)
-				TestUtil.checkObjectIsNull("checkBMICalculation: ", bmiCalculated);
-	    	else
-            	TestUtil.checkField("checkBMICalculation: ", expectedResult, bmiCalculated , 0.0000005);
+        public Scenario(Double weight, Double height, Double expectedResult) {
+            this.weight = weight;
+            this.height = height;
+            this.expectedResult = expectedResult;
         }
-	  }
+    }
 
-	  //This is an example of a Single Test in the same class with Parameterized Tests
-	  @TmsLink("TC1234_Formulas")
-	  @Feature("Goal: This test case is intended to verify the correct calculation of the different Statistical Calculations")
-	  public static class ComponentSingleTests
-	  {
-	    @Description("Calculate BMI")
-	    @Test
-	    public void checkSingleCalculation()
-	    {
-	    	Double bmiCalculated = BodyMassIndexCalculator.getBMI(73.5, 1.786);
-            TestUtil.checkField("checkBMICalculation: ", 23.0, bmiCalculated , 0.0000005);
-	    }
-	  }
+    static Scenario[] scenarios() {
+        // @formatter:off
+        return new Scenario[]{
+                new Scenario(0.0, 0.0, null),
+                new Scenario(-1.0, 0.0, null),
+                new Scenario(-1.0, 1.0, null),
+                new Scenario(1.0, -1.0, null),
+                new Scenario(1.0, 0.0, null),
+                new Scenario(5.0, 0.1, null),
+                new Scenario(30.0, 0.5, 120.0),
+                new Scenario(45.0, 1.0, 45.0),
+                new Scenario(65.0, 1.40, 33.2),
+                new Scenario(75.0, 1.8, 23.1),
+                new Scenario(95.0, 1.8, 29.3),
+                new Scenario(78.5, 1.805, 24.1)
+        };
+        // @formatter:on
+    }
+
+    @Step("Action: With a weight of {0} kg and a height of {1} meters")
+    public void printScenario(Double weight, Double height) {
+    }
+
+    @TmsLink("TC1234_Formulas")
+    @Feature("Goal: This test case is intended to verify the correct calculation of the different Statistical Calculations")
+    @ParameterizedTest
+    @MethodSource("scenarios")
+    public void checkBMICalculationForCorrectValuesTest(Scenario scenario) {
+        printScenario(scenario.weight, scenario.height);
+        Double bmiCalculated = BodyMassIndexCalculator.getBMI(scenario.weight, scenario.height);
+        if (scenario.expectedResult == null)
+            TestUtil.checkObjectIsNull("BMI", bmiCalculated);
+        else
+            TestUtil.checkField("BMI", scenario.expectedResult, bmiCalculated);
+    }
+
+    //This is an example of a Single Test in the same class with Parameterized Tests
+    @TmsLink("TC1234_Formulas")
+    @Feature("Goal: This test case is intended to verify the correct calculation of the different Statistical Calculations")
+    public static class ComponentSingleTests {
+        @Description("Calculate BMI")
+        @Test
+        public void checkSingleCalculation() {
+            Double bmiCalculated = BodyMassIndexCalculator.getBMI(73.5, 1.786);
+            TestUtil.checkField("BMI", 23.0, bmiCalculated);
+        }
+    }
 }
