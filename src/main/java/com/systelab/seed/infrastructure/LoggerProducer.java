f91875f080f19086c1dc9d9756544bf2970c0335
pep grifell <pepgrifell@gmail.com>
2019-10-23 14:59:34 +0200

    change class name to LoggerProducer

--- src/main/java/com/systelab/seed/infrastructure/LoggerProducer.java
+++ src/main/java/com/systelab/seed/infrastructure/LoggerProducer.java
@@ -2,15 +2,14 @@ package com.systelab.seed.infrastructure;
 
 import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
-import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Singleton
-public class SLF4JProducer {
+public class LoggerProducer {
 
    @Produces
    public Logger producer(InjectionPoint ip) {
-      return LoggerFactory.getLogger(ip.getMember().getDeclaringClass().getName());
+      return LoggerFactory.getLogger(
+          ip.getMember().getDeclaringClass().getName());
    }
 }
